4e596e9a42c8a658ebccdc1b18acb6a360dddfb6
ivankyrpa <ivankyrpa@gmial.com>
2012-11-13 19:52:33 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmis::Application.config.secret_token = '256ec9baaa8489743c3dba3662d2cc948c59c0b2c3f158fbbf7bd60925088cd79d6006e82b2e136106eae88b057b98dd0cee034c82a95230cb447533ffc26afd'
