1bcec31b7fe350009cb39bfc575d5f573795da38
ivankyrpa <ivankyrpa@gmial.com>
2012-11-16 15:08:24 +0400

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cmis::Application.routes.draw do
+  get "pages/about"
+
+  get "pages/contacts"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/views/pages/contacts.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "pages/contacts.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
